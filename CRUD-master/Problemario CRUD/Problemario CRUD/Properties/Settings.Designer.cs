9991dbe0a841f597b5f94409eaebe82ce74ce020
SickBoy0909 <alanomgz@gmail.com>
2019-11-16 23:32:22 -0600

    j
    i

--- CRUD-master/Problemario CRUD/Problemario CRUD/Properties/Settings.Designer.cs
+++ CRUD-master/Problemario CRUD/Problemario CRUD/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CalculadorFR.Properties
+namespace Problemario_CRUD.Properties
 {
 
 
